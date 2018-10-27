596ce1ef86dc033517f85afb4bcd9af8796ffd5e
Shishiri Nganyone <snganyon@terpmail.umd.edu>
2018-10-27 12:37:50 -0400

    php

---
+++ php/index.php
@@ -0,0 +1,10 @@
+<?php
+$config = parse_ini_file('../.git/php.ini');
+$mysqli = mysqli_connect($config['servername'], $config['username'], $config['password'], $config['dbname'], $config['port']);
+if ($mysqli->connect_errno) {
+    echo "Failed to connect to MySQL: (" . $mysqli->connect_errno . ") " . $mysqli->connect_error;
+}
+else{
+  echo "Success";
+}
+?>
