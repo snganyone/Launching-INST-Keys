ce4104ad41017fa625664ed7c5ec56b84579b474
Shishiri Nganyone <snganyon@terpmail.umd.edu>
2018-11-07 12:57:39 -0500

    Javascript

--- php/index.php
+++ php/index.php
@@ -3,6 +3,8 @@
 <head>
   <!-- Bootstrap CSS -->
 <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">
+<script src="js/script.js"></script>
+
 <?php
 $config = parse_ini_file('php.ini');
 $mysqli = mysqli_connect($config['servername'], $config['username'], $config['password'], $config['dbname'], $config['port']);
@@ -91,8 +93,5 @@ $query = $mysqli->query($sql);
 <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js" integrity="sha384-ZMP7rVo3mIykV+2+9J3UJ46jBk0WLaUAdn689aCwoqbBJiSnjAK/l8WvCWPIPm49" crossorigin="anonymous"></script>
 <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js" integrity="sha384-ChfqqxuZUCnJSK3+MXmPNIyE6ZbWh2IMqE241rYiqJxyMiZ6OW/JmZQ5stwEULTy" crossorigin="anonymous"></script>
   <!-- JQuery -->
-<script>
-$('.dropdown-toggle').dropdown();
-</script>
 </body>
 </html>
