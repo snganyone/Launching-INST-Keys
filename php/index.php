0975a560ce078e33c2c59c451528c77e8b841328
Shishiri Nganyone <snganyon@terpmail.umd.edu>
2018-11-27 11:42:32 -0500

    php

--- php/index.php
+++ php/index.php
@@ -40,7 +40,7 @@ $query = $mysqli->query($sql);
         </div>
       <div>
         <nav class="nav nav-pills nav-fill">
-        <a class="nav-item nav-link active" href="#">Active</a>
+        <a class="nav-item nav-link active" href="#">Home(Query/View) Inventory</a>
         <a class="nav-item nav-link" href="#">Edit Inventory</a>
         <a class="nav-item nav-link" href="#">Help</a>
       </nav>
@@ -90,11 +90,11 @@ $query = $mysqli->query($sql);
     </div>
     </div>
   </div>
-  <!-- Bootstrap JavaScript -->
+<!-- Bootstrap JavaScript -->
 <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
 <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js" integrity="sha384-ZMP7rVo3mIykV+2+9J3UJ46jBk0WLaUAdn689aCwoqbBJiSnjAK/l8WvCWPIPm49" crossorigin="anonymous"></script>
 <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js" integrity="sha384-ChfqqxuZUCnJSK3+MXmPNIyE6ZbWh2IMqE241rYiqJxyMiZ6OW/JmZQ5stwEULTy" crossorigin="anonymous"></script>
-  <!-- JQuery -->
+<!-- JQuery -->
 <!-- JavaScript -->
 <script src="js/script.js"></script>
 </body>
