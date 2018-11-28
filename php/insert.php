3237dd5bfc4320f7d5227a96a894872d82f1a9e4
Shishiri Nganyone <snganyon@terpmail.umd.edu>
2018-11-27 21:41:30 -0500

    php

--- php/insert.php
+++ php/insert.php
@@ -1,3 +1,39 @@
 <!DOCTYPE HTML>
 <html>
+<head>
+<!-- Bootstrap CSS -->
+<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">
+
+<?php
+$config = parse_ini_file('php.ini');
+$mysqli = mysqli_connect($config['servername'], $config['username'], $config['password'], $config['dbname'], $config['port']);
+if ($mysqli->connect_errno) {
+  echo "Failed to connect to MySQL: (" . $mysqli->connect_errno . ") " . $mysqli->connect_error;
+}
+else{
+//echo "Success";
+}
+$sql = "SELECT CONCAT(First_name, ' ', Last_name) AS employee, Building, Room_number, key_number, Core_number
+  FROM people
+  RIGHT JOIN people_has_keys
+    ON people.id_names = people_has_keys.id_names
+      LEFT JOIN inst_490.keys k
+        ON people_has_keys.id_keys = k.id_keys
+  RIGHT JOIN room r
+    ON k.id_Room = r.id_Room
+  RIGHT JOIN core c
+    ON k.id_Core = c.id_Core
+    ORDER BY Last_name DESC";
+$query = $mysqli->query($sql);
+?>
+</head>
+<body>
+<!-- Bootstrap JavaScript -->
+<script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js" integrity="sha384-ZMP7rVo3mIykV+2+9J3UJ46jBk0WLaUAdn689aCwoqbBJiSnjAK/l8WvCWPIPm49" crossorigin="anonymous"></script>
+<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js" integrity="sha384-ChfqqxuZUCnJSK3+MXmPNIyE6ZbWh2IMqE241rYiqJxyMiZ6OW/JmZQ5stwEULTy" crossorigin="anonymous"></script>
+<!-- JQuery -->
+<!-- JavaScript -->
+<script src="js/script.js"></script>
+</body>
 </html>
