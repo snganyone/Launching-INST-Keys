d78c675a981f8c379c4d278802f0eee62c9e851f
Shishiri Nganyone <snganyon@terpmail.umd.edu>
2018-11-27 21:48:17 -0500

    php

--- php/insert.php
+++ php/insert.php
@@ -28,6 +28,33 @@ $query = $mysqli->query($sql);
 ?>
 </head>
 <body>
+  <div class="container shadow-sm p-3 mb-5 bg-white rounded">
+      <div class="row">
+        <div class="col">
+        <h1>iSchool Key Inventory</h1>
+        </div>
+        <div class="col">
+        <button class="float-md-right btn btn-success">Log In</button>
+        </div>
+        </div>
+      </div>
+      <div class="container">
+        <ul class="nav nav-pills nav-fill">
+          <li class="nav-item">
+            <a class="nav-link" href="index.php">Home(Query/View) Inventory</a>
+          </li>
+          <li class="nav-item dropdown">
+            <a class="nav-link dropdown-toggle active" data-toggle="dropdown" href="#" role="button" aria-haspopup="true" aria-expanded="false">Inventory</a>
+            <div class="dropdown-menu">
+              <a class="dropdown-item" href="#">Add Inventory</a>
+              <a class="dropdown-item" href="#">Update Inventory</a>
+            </div>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="help.php">Help</a>
+          </li>
+        </ul>
+      </div>
 <!-- Bootstrap JavaScript -->
 <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
 <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js" integrity="sha384-ZMP7rVo3mIykV+2+9J3UJ46jBk0WLaUAdn689aCwoqbBJiSnjAK/l8WvCWPIPm49" crossorigin="anonymous"></script>
